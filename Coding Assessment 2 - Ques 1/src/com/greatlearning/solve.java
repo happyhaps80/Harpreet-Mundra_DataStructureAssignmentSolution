3bee6cc2e4a221e4504e76f112a7f24a6267318f
happyhaps80 <100560150+happyhaps80@users.noreply.github.com>
2022-07-31 18:13:32 +1200

    Add files via upload

---
+++ Coding Assessment 2 - Ques 1/src/com/greatlearning/solve.java
@@ -0,0 +1,34 @@
+package com.greatlearning;
+
+import java.util.*;
+public class solve {
+    public static void main(String args[]) {
+    	System.out.println("Enter the total number floors in the building : ");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+        int x[] = new int[n + 1];
+        for (int i = 1; i < n + 1; i++) {
+            System.out.println("Enter the floor size given on day : " + i);
+            int m = sc.nextInt();
+            x[m] = i;
+        }
+        int j = n;
+        boolean flag;
+        System.out.println("The order of construction is as follows ");
+        for (int i = 1; i <= n; i++) {
+            flag = false;
+            System.out.println("Day " + i + " :");
+            while (j >= 1 && x[j] <= i) {
+                flag = true;
+                System.out.print(j + " ");
+                j--;
+            }
+            if (flag == true) {
+                System.out.println();
+            }
+        }
+    }
+}
+
+
